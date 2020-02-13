25c6f94934f14163c6c4e5547439b64f7ca89266
Mayra Ramirez Duarte <mayraramirezduarte@Itexicos-MacBook-Pro.local>
2020-02-13 11:23:58 -0600

    Initial commit

---
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -0,0 +1,48 @@
+package com.eviltester.webdriver;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+
+    @Test
+    public void startWebDriver(){
+
+
+/**
+
+ The following code is for the FirefoxDriver.
+
+ You also need to download the GeckoDriver executable:
+
+ -https://github.com/mozilla/geckodriver/releases
+
+ You can easily install GeckoDriver on windows using chocolatey:
+
+ - https://chocolatey.org/packages/selenium-gecko-driver
+
+ If you are on a Mac then you could install ChromeDriver using HomeBrew
+
+ - http://brewformulas.org/geckodriver
+
+ */
+
+
+        //WebDriver driver = new FirefoxDriver();
+
+        WebDriver driver = new HtmlUnitDriver();
+
+        driver.navigate().to("https://seleniumsimplified.com");
+
+        Assert.assertTrue("title should start differently",
+                            driver.getTitle().startsWith("Selenium Simplified"));
+
+        // FirefoxDriver seems to prefer either quit or close, but sometimes throws an error if you use both
+        //driver.close();
+        driver.quit();
+    }
+
+}
